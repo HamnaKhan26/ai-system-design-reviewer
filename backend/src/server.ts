eab674b4d9fa4be6f19f0007a31f46d8749be770
HamnaKhan26 <hamnarana.faisal26@gmail.com>
2026-01-05 14:14:42 -0500

    boiler plate for system design reviewer

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-//import reviewRouter from "./routes/review";
+import reviewRouter from "./routes/review";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-//app.use("/api/review", reviewRouter);
+app.use("/api/review", reviewRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
